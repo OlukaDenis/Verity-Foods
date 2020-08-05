0f447c9bf9c0a3ee4410d6717998622163dc2628
OlukaDenis <olukadeno@gmail.com>
2020-08-06 01:23:11 +0300

    Added auth screens

---
+++ app/src/main/java/com/vecityfoods/ui/auth/AuthChooser.java
@@ -0,0 +1,16 @@
+package com.vecityfoods.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vecityfoods.R;
+
+public class AuthChooser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_chooser);
+    }
+}
